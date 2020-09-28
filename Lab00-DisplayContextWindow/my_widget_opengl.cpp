d8b70a23e9768bfba5838705e75b1e41d6924c58
Francis Daniela Mori Bautista <francis.mori@unmsm.edu.pe>
2020-09-27 22:34:38 -0500

    Carpetas

---
+++ Lab00-DisplayContextWindow/my_widget_opengl.cpp
@@ -0,0 +1,27 @@
+#include "my_widget_opengl.h"
+MiWidgetOpenGL::MiWidgetOpenGL()
+{
+
+}
+
+MiWidgetOpenGL::~MiWidgetOpenGL()
+{
+
+}
+
+void MiWidgetOpenGL::initializeGL()
+{
+
+}
+
+void MiWidgetOpenGL::resizeGL(int w, int h)
+{
+
+}
+
+void MiWidgetOpenGL::paintGL()
+{
+
+}
+
+
---
+++ Lab05.2-SimpleAnimationVersion2/my_widget_opengl.h
@@ -0,0 +1,16 @@
+#ifndef MY_WIDGET_OPENGL_H
+#define MY_WIDGET_OPENGL_H
+
+#include <QOpenGLWidget>
+#include <QOpenGLVertexArrayObject>
+#include <QOpenGLBuffer>
+#include <QOpenGLShaderProgram>
+
+class MiWidgetOpenGL: public QOpenGLWidget{
+
+public:
+    MiWidgetOpenGL();
+
+};
+
+#endif // MY_WIDGET_OPENGL_H
