c9a04ac421cd6c578d4fc9e5da69772e134b3108
Francis Daniela Mori Bautista <francis.mori@unmsm.edu.pe>
2020-09-30 03:24:44 -0500

    VertexFragment

--- Lab03-PointVertexFragmentShader/my_widget_opengl.cpp
+++ Lab03-PointVertexFragmentShader/my_widget_opengl.cpp
@@ -13,12 +13,32 @@ MiWidgetOpenGL::~MiWidgetOpenGL()
 
 void MiWidgetOpenGL::initializeGL()
 {
+    sp = new QOpenGLShaderProgram();
+    sp->addShaderFromSourceCode(QOpenGLShader::Vertex,
+                                "#version 410 \n"
+                                "in vec3 position;\n"
+                                "void main(){\n"
+                                "gl_Position = vec4(0.4*position, 1.0);"
+                                "}");
+    sp->addShaderFromSourceCode(QOpenGLShader::Fragment,
+                                "#version 410 \n"
+                                "out vec4 color; \n"
+                                "void main(void) { \n"
+                                "color = vec4(1.0, 0.0, 0.0, 1.0);"
+                                "}");
+    sp->link();
+    float vertices[]={
+        -1.0f, -1.0f, 0.0f,
+         1.0f, -1.0f, 0.0f,
+         0.0f, 1.0f,  0.0f,
+    };
     vao.create();
     vao.bind();
 
     vbo.create();
     vbo.setUsagePattern(QOpenGLBuffer::StaticDraw);
     vbo.bind();
+    vbo.allocate(vertices,sizeof(vertices));
 
     vao.release();
     vbo.release();
@@ -31,10 +51,13 @@ void MiWidgetOpenGL::resizeGL(int w, int h)
 
 void MiWidgetOpenGL::paintGL()
 {
+    sp->bind();
     vao.bind();
     vbo.bind();
+    sp->enableAttributeArray("position");
+    sp->setAttributeArray("position",GL_FLOAT,0,3);
     glClearColor(r, g, b, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+    glPointSize(20.f);
+    glDrawArrays(GL_POINTS,0,3);
 }
-
-
--- Lab04-0-TriangleVertexFragmentShaders/my_widget_opengl.cpp
+++ Lab04-0-TriangleVertexFragmentShaders/my_widget_opengl.cpp
@@ -13,13 +13,39 @@ MiWidgetOpenGL::~MiWidgetOpenGL()
 
 void MiWidgetOpenGL::initializeGL()
 {
+    sp = new QOpenGLShaderProgram();
+    sp->addShaderFromSourceCode(QOpenGLShader::Vertex,
+                                "#version 450\n"
+                                "in vec3 position;\n"
+                                "layout (location = 1)in vec3 color;\n"
+                                "layout (location = 0)out vec4 fragColor;\n"
+                                "void main(){\n"
+                                "fragColor=vec4(color,1.0);\n"
+                                "gl_Position = vec4(position,1.0);\n"
+                                "}");
+    sp->addShaderFromSourceCode(QOpenGLShader::Fragment,
+                                "#version 450\n"
+                                "in vec4 fragColor;\n"
+                                "out vec4 fColor;\n"
+                                "void main(){\n"
+                                "fColor = fragColor;\n"
+                                "}");
+    sp->link();
+    float vertices[]={
+        -0.5f, -0.5f,0.0f,
+         0.5f, -0.5f,0.0f,
+         0.0f, 0.5f, 0.0f,
+         1.0f, 0.0f, 0.0f,
+         0.0f, 1.0f, 0.0f,
+         0.0f, 0.0f, 1.0f,
+    };
     vao.create();
     vao.bind();
 
     vbo.create();
     vbo.setUsagePattern(QOpenGLBuffer::StaticDraw);
     vbo.bind();
-
+    vbo.allocate(vertices,sizeof(vertices));
     vao.release();
     vbo.release();
 }
@@ -31,11 +57,16 @@ void MiWidgetOpenGL::resizeGL(int w, int h)
 
 void MiWidgetOpenGL::paintGL()
 {
+    sp->bind();
     vao.bind();
     vbo.bind();
+    sp->enableAttributeArray("position");
+    sp->setAttributeArray("position",GL_FLOAT,0,3);
+    sp->enableAttributeArray("color");
+    sp->setAttributeArray("color",GL_FLOAT,(void*)(9*sizeof(float)),3);
     glClearColor(r, g, b, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT);
-
+    glDrawArrays(GL_TRIANGLES,0,3);
 }
 
 
